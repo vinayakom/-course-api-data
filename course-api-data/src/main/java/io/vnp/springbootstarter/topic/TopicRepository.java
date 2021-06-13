b34009f7582b5ab3ac55daea7494d7ccd2f34f78
Vinayak <vinayakom@gmail.com>
2021-06-13 20:03:15 +0530

    Data commits

---
+++ course-api-data/src/main/java/io/vnp/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package io.vnp.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+	
+}
