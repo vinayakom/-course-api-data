95d778a3168462e0ded49e674863af8ee75ee217
Vinayak <vinayakom@gmail.com>
2021-06-13 20:15:44 +0530

    Db details

--- course-api-data/src/main/java/io/vnp/springbootstarter/topic/TopicService.java
+++ course-api-data/src/main/java/io/vnp/springbootstarter/topic/TopicService.java
@@ -1,7 +1,6 @@
 package io.vnp.springbootstarter.topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +12,7 @@ public class TopicService {
 	@Autowired
 	private TopicRepository topicRepository;
 	
-	private List<Topic> topics = new ArrayList<> (Arrays.asList(
-			new Topic("Spring","Spring Framework","Spring Framework Description"),
-			new Topic("Java","Core Java","Core Java Description"),
-			new Topic("Javascript","Javascript","Javascript Description")
-			));
-
 	public List<Topic> getAllTopics() {
-		//return topics;
 		List<Topic> topics = new ArrayList<>();
 		topicRepository.findAll()
 				.forEach(topics::add);
@@ -28,28 +20,20 @@ public class TopicService {
 	}
 	
 	public Topic getTopic(String id) {
-		//return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
 		return topicRepository.findById(id).get();
 	}
 
 	public void addTopic(Topic topic) {
-		//topics.add(topic);
 		topicRepository.save(topic);
 		
 	}
 
 	public void updateTopic(String id, Topic topic) {
-		for(int i=0; i < topics.size(); i++) {
-			Topic t = topics.get(i);
-			if (t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
+		topicRepository.save(topic);
 		
 	}
 
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
 	}
 }
